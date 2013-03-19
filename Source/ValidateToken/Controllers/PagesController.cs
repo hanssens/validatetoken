fcaef64d7e8d57e8d38a42511f2eb52f73efec39
Juliën Hanssens <j@hanssens.com>
2013-03-19 13:22:56 +0100

    Initial checkin of the project
    - Included working AJAX sample, that posts a sessionkey through the
    header
    - SessionKey is validated using an attribute

---
+++ Source/ValidateToken/Controllers/PagesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ValidateToken.Controllers
+{
+    public class PagesController : Controller
+    {
+        //
+        // GET: /Pages/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [RequireSessionKey("SessionKey")]
+        public ActionResult AuthorizedCall()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Source/ValidateToken/Filters/RequireSessionKey.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ValidateToken
+{
+    public class RequireSessionKey : ActionFilterAttribute
+    {
+        public string TokenFieldName { get; set; }
+
+        public RequireSessionKey(string tokenFieldName)
+        {
+            this.TokenFieldName = tokenFieldName;
+        }
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            string token = null;
+
+            try
+            {
+                // Fetch the provided token
+                if (filterContext != null)
+                    token = filterContext.HttpContext.Request.Headers.GetValues(TokenFieldName).FirstOrDefault();
+            }
+            catch (ArgumentException)
+            {
+                // Token/SessionKey hasn't been provided
+            }
+            catch (Exception ex)
+            {
+                // Something awful happened here...
+                throw ex;
+            }
+
+            // Validate token
+            if (!IsValidToken(token))
+                throw new UnauthorizedAccessException("Invalid token");
+
+            // Finish executing the action
+            base.OnActionExecuting(filterContext);
+        }
+
+        public virtual bool IsValidToken(string token)
+        {
+            if (String.IsNullOrEmpty(token))
+                return false;
+
+            return (token == "12345");
+        }
+
+    }
+}
